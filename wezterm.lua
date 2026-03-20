a72fd4472f521506f19e84bfcb825b3bd0075602
johncming <johncming@126.com>
2026-03-20 12:55:44 +0800

    up

--- wezterm.lua
+++ wezterm.lua
@@ -446,7 +446,7 @@ config.enable_scroll_bar = false -- 隐藏右侧滚动条
 -- ============================================================
 config.window_background_opacity = 0.97
 
-config.window_background_image = "/Users/john/Pictures/bg/xcmj.jpg"
+config.window_background_image = "/Users/john/Pictures/bg/girl.gif"
 
 -- 降低背景图片亮度，让文字更清晰
 config.window_background_image_hsb = {
